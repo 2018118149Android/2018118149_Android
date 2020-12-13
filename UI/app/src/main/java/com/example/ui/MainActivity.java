baa6fdf9fcdcf23c86887213f168e520b9f83230
asus@LAPTOP-KG7RLSV0 <1653659056@qq.com>
2020-12-13 10:32:45 +0800

    UI控件和布局

---
+++ UI/app/src/main/java/com/example/ui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ui;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public void onClick(View v){
+    }
+}
